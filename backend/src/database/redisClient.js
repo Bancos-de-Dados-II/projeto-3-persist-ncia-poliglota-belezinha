2e17a47f4a46fb02b0ae41392648722ab9933a6b
adeilton <adeutoo@gmail.com>
2025-03-09 11:14:39 -0300

    consertando url

--- backend/src/database/redisClient.js
+++ backend/src/database/redisClient.js
@@ -3,7 +3,7 @@ import { createClient } from 'redis';
 import dotenv from 'dotenv'
 dotenv.config()
 const redisClient = createClient({
-  url: process.env.REDIS_URL // Ajuste a URL conforme necessário
+  url: process.env.REDIS_URL 
 });
 
 redisClient.on('connect', () => {
@@ -16,5 +16,4 @@ redisClient.on('error', (err) => {
 
 redisClient.connect();
 
-// Exporta a instância do redisClient para ser utilizada em outros arquivos
 export { redisClient };
